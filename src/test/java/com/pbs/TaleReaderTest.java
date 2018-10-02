a8633f219db76fd3d2c6ae7c00964f9275e9830a
Gajendra Bisht <gajendrabisht@gmail.com>
2018-10-02 14:21:15 +0100

    read file input

--- src/test/java/com/pbs/TaleReaderTest.java
+++ src/test/java/com/pbs/TaleReaderTest.java
@@ -2,6 +2,9 @@ package com.pbs;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -35,6 +38,14 @@ public class TaleReaderTest {
         assertThat(report.get("there"), is(1));
         assertThat(report.get("here"), is(1));
         assertThat(report.get("everyone"), is(1));
+    }
+
+    @Test
+    public void shouldReadFromLargeFile() throws IOException {
+        Map<String, Integer> report = taleReader.read(Files.lines(Paths.get("src/test/resources/input.txt")));
+        assertThat(report.get("the"), is(81));
+        assertThat(report.get("a"), is(31));
+        assertThat(report.get("and"), is(55));
     }
 
 }
