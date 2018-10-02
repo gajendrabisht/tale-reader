fa4453623af98acb265594af9521281699aa03d0
Gajendra Bisht <gajendrabisht@gmail.com>
2018-10-02 14:04:58 +0100

    Multiple occurences

--- src/test/java/com/pbs/TaleReaderTest.java
+++ src/test/java/com/pbs/TaleReaderTest.java
@@ -19,4 +19,13 @@ public class TaleReaderTest {
         assertThat(report.get("there"), is(1));
     }
 
+    @Test
+    public void shouldReturnMultipleOccurrences() {
+        Map<String, Integer> report = taleReader.read(Arrays.asList("hello here hello there hello everyone").stream());
+        assertThat(report.get("hello"), is(3));
+        assertThat(report.get("there"), is(1));
+        assertThat(report.get("here"), is(1));
+        assertThat(report.get("everyone"), is(1));
+    }
+
 }
